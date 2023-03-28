f13e2d7373afaf833359bcfcf2f159611694dddb
Barbara Pecsenye <barbara.pecsenye@stud.uni-corvinus.hu>
2023-03-28 08:43:10 +0200

    GetRates függvény (3.feladat)

--- Gyak3_VersionControl_K158DZ/6.gyak_K158DZ/Form1.cs
+++ Gyak3_VersionControl_K158DZ/6.gyak_K158DZ/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _6.gyak_K158DZ.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,23 @@ namespace _6.gyak_K158DZ
         public Form1()
         {
             InitializeComponent();
+            GetRates();
+        }
+
+        private void GetRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient(); //de lehetne MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient() is
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
