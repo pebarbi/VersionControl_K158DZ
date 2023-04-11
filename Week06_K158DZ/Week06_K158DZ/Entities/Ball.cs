c67a6c4aed82ca111556a94d1da3b5a4f6390911
Barbara Pecsenye <barbara.pecsenye@stud.uni-corvinus.hu>
2023-04-11 08:31:01 +0200

    Labda létrehozása (Ball osztály)

---
+++ Week06_K158DZ/Week06_K158DZ/Entities/Ball.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week06_K158DZ.Entities
+{
+    public class Ball : Label
+    {
+        public Ball()
+        {
+            Width = 50;
+            Height = Width;
+            Paint += Ball_Paint;
+            AutoSize = false;
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected void DrawImage(Graphics g)
+        {
+            //var brush = new SolidBrush(Color.Blue);
+            //g.FillEllipse(brush, 0, 0, Width, Height);
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+        public void MoveBall()
+        {
+            Left += 1;
+        }
+    }
+}
