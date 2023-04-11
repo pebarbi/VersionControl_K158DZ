cbcb5f6ad2c5c58e449597ce97d1dcf5ccb9e227
Barbara Pecsenye <barbara.pecsenye@stud.uni-corvinus.hu>
2023-04-11 09:29:03 +0200

    Termékválasztó gombok

--- Week06_K158DZ/Week06_K158DZ/Form1.Designer.cs
+++ Week06_K158DZ/Week06_K158DZ/Form1.Designer.cs
@@ -33,13 +33,19 @@ namespace Week06_K158DZ
             this.mainPanel = new System.Windows.Forms.Panel();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.btnSelectCar = new System.Windows.Forms.Button();
+            this.btnSelectBall = new System.Windows.Forms.Button();
+            this.lblNext = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // mainPanel
             // 
-            this.mainPanel.Location = new System.Drawing.Point(12, 12);
+            this.mainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainPanel.Location = new System.Drawing.Point(12, 127);
             this.mainPanel.Name = "mainPanel";
-            this.mainPanel.Size = new System.Drawing.Size(776, 355);
+            this.mainPanel.Size = new System.Drawing.Size(776, 418);
             this.mainPanel.TabIndex = 0;
             // 
             // createTimer
@@ -54,15 +60,48 @@ namespace Week06_K158DZ
             this.conveyorTimer.Interval = 10;
             this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick);
             // 
+            // btnSelectCar
+            // 
+            this.btnSelectCar.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectCar.Name = "btnSelectCar";
+            this.btnSelectCar.Size = new System.Drawing.Size(75, 75);
+            this.btnSelectCar.TabIndex = 1;
+            this.btnSelectCar.Text = "Car";
+            this.btnSelectCar.UseVisualStyleBackColor = true;
+            this.btnSelectCar.Click += new System.EventHandler(this.btnSelectCar_Click);
+            // 
+            // btnSelectBall
+            // 
+            this.btnSelectBall.Location = new System.Drawing.Point(93, 12);
+            this.btnSelectBall.Name = "btnSelectBall";
+            this.btnSelectBall.Size = new System.Drawing.Size(75, 75);
+            this.btnSelectBall.TabIndex = 2;
+            this.btnSelectBall.Text = "Ball";
+            this.btnSelectBall.UseVisualStyleBackColor = true;
+            this.btnSelectBall.Click += new System.EventHandler(this.btnSelectBall_Click);
+            // 
+            // lblNext
+            // 
+            this.lblNext.AutoSize = true;
+            this.lblNext.Location = new System.Drawing.Point(174, 12);
+            this.lblNext.Name = "lblNext";
+            this.lblNext.Size = new System.Drawing.Size(65, 13);
+            this.lblNext.TabIndex = 3;
+            this.lblNext.Text = "Coming next";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 557);
+            this.Controls.Add(this.lblNext);
+            this.Controls.Add(this.btnSelectBall);
+            this.Controls.Add(this.btnSelectCar);
             this.Controls.Add(this.mainPanel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,6 +110,9 @@ namespace Week06_K158DZ
         private System.Windows.Forms.Panel mainPanel;
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Button btnSelectCar;
+        private System.Windows.Forms.Button btnSelectBall;
+        private System.Windows.Forms.Label lblNext;
     }
 }
 
