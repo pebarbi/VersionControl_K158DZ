7fa38f57f0c2864ea0e35867349759c45d30398f
Barbara Pecsenye <barbara.pecsenye@stud.uni-corvinus.hu>
2023-04-11 08:35:03 +0200

    Labdagyár létrehozása (BallFactory osztály)

---
+++ Week06_K158DZ/Week06_K158DZ/Entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week06_K158DZ.Entities
+{
+    public class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
