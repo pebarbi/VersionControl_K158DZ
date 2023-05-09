940d517c7b369c9ea309e8806ce8a390d1ea15d7
Barbara Pecsenye <barbara.pecsenye@stud.uni-corvinus.hu>
2023-05-09 08:26:23 +0200

    public enum

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp1.Entities;
 
 namespace WindowsFormsApp1
 {
@@ -15,6 +16,15 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+
+            Gender g = Gender.Female; //így kell létrehozni egy enumeráció pédányosítást, mert ez nem osztály!!!
+            //pl
+            if (g == Gender.Female)
+            {
+
+            }
+
+            var x = (Gender)2; //így lehet castolni kódszámokkal
         }
     }
 }
