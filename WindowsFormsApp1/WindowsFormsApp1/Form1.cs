c8ca33a1ffa6a21141ecc450d752b7f7905f6ddb
Barbara Pecsenye <barbara.pecsenye@stud.uni-corvinus.hu>
2023-05-09 09:05:09 +0200

    jav adatok betöltése

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,18 +14,84 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        List<BirthProbability> Population = new List<BirthProbability>();
+        List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+        List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+
         public Form1()
         {
             InitializeComponent();
 
-            Gender g = Gender.Female; //így kell létrehozni egy enumeráció pédányosítást, mert ez nem osztály!!!
-            //pl
-            if (g == Gender.Female)
-            {
+            //Gender g = Gender.Female; //így kell létrehozni egy enumeráció pédányosítást, mert ez nem osztály!!!
+            ////pl
+            //if (g == Gender.Female)
+            //{
 
+            //}
+
+            //var x = (Gender)2; //így lehet castolni kódszámokkal
+
+            Population = GetPopulation(@"C:\Tempp\nép.csv");
+            BirthProbabilities = GetBirthProbabilities(@"C:\Tempp\születés.csv");
+            DeathProbabilities = GetDeathProbabilities(@"C:\Tempp\halál.csv");
+        }
+
+        public List<BirthProbability> GetPopulation(string csvpath)
+        {
+            var population = new List<BirthProbability>();
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    population.Add(new BirthProbability()
+                    {
+                        BirthYear = int.Parse(line[0]),
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                        NbrOfChildren = byte.Parse(line[2])
+                    });
+                }
             }
 
-            var x = (Gender)2; //így lehet castolni kódszámokkal
+            return population;
+        }
+
+        public List<BirthProbability> GetBirthProbabilities(string csvpath)
+        {
+            var birthProbabilities = new List<BirthProbability>();
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    birthProbabilities.Add(new BirthProbability()
+                    {
+                        Age = int.Parse(line[0]),
+                        NbrOfChildren = byte.Parse(line[1]),
+                        P= double.Parse(line[2])
+                    });
+                }
+            }
+            return birthProbabilities;
+        }
+
+        public List<DeathProbability> GetDeathProbabilities(string csvpath)
+        {
+            var deathProbabilities = new List<DeathProbability>();
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    deathProbabilities.Add(new BirthProbability()
+                    {
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[0]),
+                        Age = int.Parse(line[1]),
+                        P = double.Parse(line[2])
+                    });
+                }
+            }
+            return deathProbabilities;
         }
     }
 }
