1772531c92d1960a73eb190efa478edc92a3ce4d
Barbara Pecsenye <barbara.pecsenye@stud.uni-corvinus.hu>
2023-05-09 09:18:49 +0200

    Szimulációs lépés, javítások és random generátor

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -14,7 +14,8 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        List<BirthProbability> Population = new List<BirthProbability>();
+        Random rng = new Random(1234);
+        List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
 
@@ -50,7 +51,8 @@ namespace WindowsFormsApp1
                                   where x.Gender == Gender.Female && x.IsAlive
                                   select x).Count();
 
-                Console.WriteLine(string.Format("Év:{0}\t" "Férfiak száma: {1}\t" "Nők száma: {2}\t", year, nbrOfMales, nbrOfFemales));
+                //Console.WriteLine(string.Format("Év:{0}\t" "Férfiak száma: {1}\t" "Nők száma: {2}\t", year, nbrOfMales, nbrOfFemales));
+                Console.WriteLine(string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
             }
         }
 
@@ -101,7 +103,7 @@ namespace WindowsFormsApp1
                 while (!sr.EndOfStream)
                 {
                     var line = sr.ReadLine().Split(';');
-                    deathProbabilities.Add(new BirthProbability()
+                    deathProbabilities.Add(new DeathProbability()
                     {
                         Gender = (Gender)Enum.Parse(typeof(Gender), line[0]),
                         Age = int.Parse(line[1]),
@@ -110,6 +112,42 @@ namespace WindowsFormsApp1
                 }
             }
             return deathProbabilities;
+        }
+
+        private void SimStep(int year, Person person)
+        {
+            //Ha halott akkor kihagyjuk, ugrunk a ciklus következő lépésére
+            if (!person.IsAlive) return;
+
+            // Letároljuk az életkort, hogy ne kelljen mindenhol újraszámolni
+            byte age = (byte)(year - person.BirthYear);
+
+            // Halál kezelése
+            // Halálozási valószínűség kikeresése
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.P).FirstOrDefault();
+            // Meghal a személy?
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+            //Születés kezelése - csak az élő nők szülnek
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                //Szülési valószínűség kikeresése
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.P).FirstOrDefault();
+                //Születik gyermek?
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
         }
     }
 }
