039a0211462b8cff65f1f8d67bb757877eb9e5cc
Barbara Pecsenye <barbara.pecsenye@stud.uni-corvinus.hu>
2023-05-16 08:26:39 +0200

    Child class

---
+++ MintaZH_3/MintaZH_3/Entities/Child.cs
@@ -0,0 +1,28 @@
+﻿using MintaZH_3.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MintaZH_3.Entities
+{
+    public class Child
+    {
+        public string Name { get; set; }
+        public Behaviour YearlyBehaviour { get; set; }
+
+        public bool CheckBehaviour(int value)
+        {
+            //if (value <=1 && value>=5)
+            //{
+            //    return true;
+            //}
+            //return false;
+
+            //return true;
+
+            return value <= 1 && value >= 5;
+        }
+    }
+}
