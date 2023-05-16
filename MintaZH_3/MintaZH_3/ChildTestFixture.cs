2f96109eddbae4a84b96bca47cbbc509f2219723
Barbara Pecsenye <barbara.pecsenye@stud.uni-corvinus.hu>
2023-05-16 09:29:58 +0200

    unittest

---
+++ MintaZH_3/MintaZH_3/ChildTestFixture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MintaZH_3
+{
+    public class ChildTestFixture
+    {
+        [Test]
+
+        public void TestCheckBehaviour (int value, bool expectedResult)
+        {
+            var c = new Child();
+        }
+    }
+}
---
+++ MintaZH_3/UnitTestProject1/ChildTestFixture.cs
@@ -0,0 +1,34 @@
+﻿using MintaZH_3.Entities;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MintaZH_3
+{
+    public class ChildTestFixture
+    {
+        [   Test,
+            TestCase 1,
+            TestCase 2,
+            TestCase 3,
+            TestCase 4,
+            TestCase 5,
+            TestCase 555
+        ]
+
+        public void TestCheckBehaviour (int value, bool expectedResult)
+        {
+            //Arrange
+            var c = new Child();
+
+            //Act
+            var actualResult = c.CheckBehaviour(value);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
