2ea096d76d9201d2b65a38515e2fb3e4d5a5e297
Barbara Pecsenye <barbara.pecsenye@stud.uni-corvinus.hu>
2023-05-16 09:12:53 +0200

    fájlba írás

--- MintaZH_3/MintaZH_3/Form1.cs
+++ MintaZH_3/MintaZH_3/Form1.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -54,9 +55,31 @@ namespace MintaZH_3
             var sfd = new SaveFileDialog();
             sfd.InitialDirectory = Application.StartupPath; //!!!!!!!!!
 
-            if (sfd.ShowDialog != DialogResult.OK)
+            if (sfd.ShowDialog() != DialogResult.OK)
             {
                 return;
+            }
+
+            using (var sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                sw.WriteLine("Név; Ajándék");
+                foreach (var c in children)
+                {
+                    sw.WriteLine(c.Name);
+                    sw.Write(";");
+
+                    var gifts = "";
+                    for (int i = 0; i < c.Gifts.Count; i++)
+                    {
+                        gifts += c.Gifts[i].Name;
+                        if (i < c.Gifts.Count - 1)
+                        {
+                            gifts += " ";
+                        }
+                    }
+                    sw.Write(gifts);
+                    //sw.Write("\n");
+                }
             }
         }
     }
