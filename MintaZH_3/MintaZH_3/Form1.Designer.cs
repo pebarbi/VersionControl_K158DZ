4e52f70720908f9e4af5d48c92181ab8cda9e6bd
Barbara Pecsenye <barbara.pecsenye@stud.uni-corvinus.hu>
2023-05-16 08:34:16 +0200

    form1Design

--- MintaZH_3/MintaZH_3/Form1.Designer.cs
+++ MintaZH_3/MintaZH_3/Form1.Designer.cs
@@ -29,13 +29,117 @@ namespace MintaZH_3
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtBehaviour = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.lblBadCount = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(91, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(122, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtBehaviour
+            // 
+            this.txtBehaviour.Location = new System.Drawing.Point(91, 57);
+            this.txtBehaviour.Name = "txtBehaviour";
+            this.txtBehaviour.Size = new System.Drawing.Size(122, 20);
+            this.txtBehaviour.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Név:";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(13, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Viselkedés:";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(42, 116);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(123, 23);
+            this.buttonAdd.TabIndex = 4;
+            this.buttonAdd.Text = "Hozzáadás";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // lblBadCount
+            // 
+            this.lblBadCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblBadCount.Location = new System.Drawing.Point(216, 420);
+            this.lblBadCount.Name = "lblBadCount";
+            this.lblBadCount.Size = new System.Drawing.Size(98, 23);
+            this.lblBadCount.TabIndex = 5;
+            this.lblBadCount.Text = "Rosszak száma: 0";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(219, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(553, 390);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStart.Location = new System.Drawing.Point(697, 415);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 7;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblBadCount);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBehaviour);
+            this.Controls.Add(this.txtName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtBehaviour;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label lblBadCount;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonStart;
     }
 }
 
